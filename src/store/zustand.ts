90eec79e07f027182058e83fa60768c032c37c57
JorgeAssaf <jorgeassaf160@gmail.com>
2023-03-13 11:39:29 -0600

    refactor: 💡 zutand store

--- src/store/zustand.ts
+++ src/store/zustand.ts
@@ -1,50 +1,45 @@
 import axios from 'axios'
 import { create } from 'zustand'
+import { Details } from './../types/indext'
 interface CategoriaState {
   categoria: string[]
-  addCategoria: (categoria: string) => void
   fetchCategoria: () => Promise<void>
 }
 
 interface BebidaState {
   bebida: any[]
   id: string
-  details: any[]
-  addBebida: (bebida: any) => void
+  details: Details[]
   getId: (id: any) => void
   getBebida: (bebida: { nombre: string; categoria: string }) => Promise<void>
   getDatils: (id: any) => Promise<void>
-  getIngredientes: (id: any) => Promise<void>
-
+  resetDetails: () => void
 }
-
-
 
 export const useCategoria = create<CategoriaState>((set) => ({
   categoria: [],
-  addCategoria: (categoria: string) => set((state) => ({ categoria: [...state.categoria, categoria] })),
+
   fetchCategoria: async () => {
     const { data } = await axios(
       'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
     )
 
-    set({ categoria: data.drinks })
+    set({ categoria: data?.drinks })
   },
 }))
 
 export const useBebida = create<BebidaState>((set) => ({
-
   bebida: [],
   details: [],
   id: '',
-  addBebida: (bebida: any) => set((state) => ({ bebida: [...state.bebida, bebida] })),
-  getBebida: async (bebida) => {
+  ingredientes: [],
+  resetDetails: () => set((state) => ({ details: (state.details = []) })),
+  getBebida: async ({ categoria = '', nombre }) => {
     try {
-      const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${bebida?.nombre!}&c=${bebida?.categoria!}`
-      if (bebida === null) return
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre}${categoria?.length > 0 ? `&c=${categoria}` : ''
+        }`
       const { data } = await axios(url)
       set({ bebida: data?.drinks })
-
     } catch (error) {
       console.log(error)
     }
@@ -55,18 +50,9 @@ export const useBebida = create<BebidaState>((set) => ({
     try {
       const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
       const { data } = await axios(url)
-      set({ details: data.drinks })
+      set({ details: data?.drinks })
     } catch (error) {
       console.log(error)
     }
   },
-  getIngredientes: async (id) => {
-    try {
-      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
-      const { data } = await axios(url)
-      set({ bebida: data.drinks })
-    } catch (error) {
-      console.log(error)
-    }
-  }
 }))
